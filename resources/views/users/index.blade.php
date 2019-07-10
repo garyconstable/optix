5b573b275165ee4e57c20b69dd63994a6260c455
Gary Constable <garyconstable80@gmail.com>
2019-07-10 23:04:49 +0100

    Add user link

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,13 +10,15 @@
                         <tr>
                             <th scope="col">Name</th>
                             <th scope="col">Last Updated</th>
+                            <th scope="col">&nbsp;</th>
                         </tr>
                     </thead>
                     <tbody>
                     @foreach ($data['users'] as $user)
                         <tr>
-                            <th scope="row">{{ $user->name }}</th>
-                            <td>{{ $user->updated_at }}</td>
+                            <td class="align-middle">{{ $user->name }}</td>
+                            <td class="align-middle">{{ $user->updated_at }}</td>
+                            <td class="align-middle, text-right"><a class="btn btn-primary" href="/user/{{ $user->id }}">View user</a></td>
                         </tr>
                     @endforeach
                     </tbody>
