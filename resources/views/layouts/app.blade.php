095e0e77a5b887af0907c83061e87894e75d0b94
Gary Constable <garyconstable80@gmail.com>
2019-07-11 21:35:00 +0100

    NPM install and move styles to sass file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,62 +7,39 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <style>
-        .alert {
-            z-index: 99;
-            top: 60px;
-            right: 18px;
-            min-width: 30%;
-            position: fixed;
-            animation: slide 0.5s forwards;
-        }
-
-        @keyframes slide {
-            100% {
-                top: 30px;
-            }
-        }
-
-        @media screen and (max-width: 668px) {
-            .alert {
-                left: 10px;
-                right: 10px;
-            }
-        }
-    </style>
     <title>Optix - Dev Test</title>
 </head>
 <body>
-@include('inc.navbar')
-<main class="container mt-4">
-    @yield('content')
-</main>
+    @include('inc.navbar')
+    <main class="container mt-4">
+        @yield('content')
+    </main>
 
-<script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
 
-@if(session('status'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{session('status')}}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-@endif
+    @if(session('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{session('status')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
 
-@if(session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{session('error')}}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-@endif
-<script>
-    $(document).ready(function () {
-        setTimeout(function () {
-            $(".alert").alert('close');
-        }, 3000);
-    });
-</script>
+    @if(session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{session('error')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                $(".alert").alert('close');
+            }, 3000);
+        });
+    </script>
 </body>
 </html>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <!-- Add icon library -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <div class="card mt-5 mb-5">
         <img src="https://garyconstable.dev/assets/images/img_avatar.png" alt="John" style="width:100%">
         <h1 class="my-5">{{$data['user'][0]->name}}</h1>
@@ -16,35 +12,4 @@
             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-
-    <style>
-
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            max-width: 300px;
-            margin: 0 auto 0 auto;
-            text-align: center;
-        }
-
-        .title {
-            color: grey;
-            font-size: 18px;
-        }
-
-        button {
-            border: none;
-            outline: 0;
-            display: inline-block;
-            padding: 8px;
-            color: white;
-            background-color: #000;
-            text-align: center;
-            cursor: pointer;
-            width: 100%;
-            font-size: 18px;
-        }
-
-    </style>
-
-
 @endsection
