eb0d665b58380c85552aa5786b024583f101165a
Gary Constable <garyconstable80@gmail.com>
2019-07-13 18:17:33 +0100

    Add Faker and Data.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Comment;
+use App\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 1000; $i++) {
+            App\User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Str::random(10),
+                'biography' => Str::random(200),
+            ]);
+        }
+
+        for ($i = 0; $i < 1000; $i++) {
+            App\Comment::create([
+                'comment' => Str::random(200),
+                'id_for' => $faker->numberBetween(1, 1000),
+                'id_by' => $faker->numberBetween(1, 1000)
+            ]);
+        }
     }
 }
